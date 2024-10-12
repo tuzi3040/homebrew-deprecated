b2f17aed00d832929824561e12d20a9230f790e7
tuzi3040 <28290621+tuzi3040@users.noreply.github.com>
2024-10-12 14:57:24 +0000

    googler: add 4.3.13 bottle.

--- Formula/googler.rb
+++ Formula/googler.rb
@@ -8,6 +8,13 @@ class Googler < Formula
   license "GPL-3.0-or-later"
   head "https://github.com/oksiquatzel/googler.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/tuzi3040/bottles"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d105b8a2c2d540d52ad2253bf02ab51c88e5a4cf56455d586fbf92765129dc68"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9b8d1cb5770dab9b25bf938289fbfb85e900448455e23fb9eff64af87fd1e089"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "097790b17dc72bdd7444b8ee3db0bc417581e3f430de02f2b938d70368d263a4"
+  end
+
   depends_on "python@3.12"
 
   def install
